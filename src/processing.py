8c7b9839ef74f4b9000c68629bab093ed48c6aad
Danil Samusev <samusevdanil@gmail.com>
2025-03-09 17:35:53 +0900

    Реализовал функцию сортировки по дате

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def filter_by_state(dicts: list, state: str = "EXECUTED") -> list:
     """Возвращает все словари с нужным значением ключа 'state'"""
 
@@ -8,3 +11,15 @@ def filter_by_state(dicts: list, state: str = "EXECUTED") -> list:
             needed_list.append(item)
 
     return needed_list
+
+
+def sort_by_date(dicts: list, reverse: bool = True) -> list:
+    """Возвращает список, отсортированный по дате"""
+
+    sorted_list = sorted(
+        dicts,
+        key=lambda x: datetime.strptime(x["date"], "%Y-%m-%dT%H:%M:%S.%f"),
+        reverse=reverse,
+    )
+
+    return sorted_list
